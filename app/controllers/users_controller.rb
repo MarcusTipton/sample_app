88267a7a7ddaf797db9b4ba4542a004cbbbbd5fe
Marcus Tipton <deyontip@live.com>
2014-09-24 09:23:13 -0500

    Just did 10.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,13 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  # def show
+  #   @user = User.find(params[:id])
+  # end
+
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -54,12 +59,7 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    
 
     def correct_user
       @user = User.find(params[:id])
